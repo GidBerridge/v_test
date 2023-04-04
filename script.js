f25656abb96a2e6fa76d5cedab111db40f055f9d
Gid <gid@Gids-iMac.local>
2023-04-04 15:27:41 +0100

    css_tidied

--- script.js
+++ script.js
@@ -3,20 +3,39 @@ const modalContainer = document.getElementById("modal-container");
 const closeModal = document.getElementById("close-modal-X");
 const closeModalButton = document.getElementById("close-modal-button");
 
+const removeShow = () => {
+    modalContainer.classList.remove("show")
+    setTimeout(function () {
+        modalContainer.style.display = "none"
+    }, 210);
+}
+
 modalButton.addEventListener("click", function (event) {
     event.preventDefault();
-    modalContainer.style.display = "block";
-});
+
+    if (modalContainer.style.display == "block") {
+        removeShow()
+    } else {
+        modalContainer.style.display = "block";
+        setTimeout(function () {
+            modalContainer.classList.add("show")
+        }, 20);
+    }
+
+}, false);
 
 closeModal.addEventListener("click", function () {
-    modalContainer.style.display = "none";
+    removeShow()
 });
+
+
 closeModalButton.addEventListener("click", function () {
-    modalContainer.style.display = "none";
+    removeShow()
 });
 
 window.addEventListener("click", function (event) {
     if (event.target == modalContainer) {
-        modalContainer.style.display = "none";
+        removeShow()
     }
 });
+
